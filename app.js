6fe9ce5a559fdf11c77e8320f2df1f6ef209fc43
germandeburzaco <germandeburzaco@hotmail.com>
2021-10-07 18:38:58 -0300

    1

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 
 
-
-
-
 btnModo.addEventListener('click', (e) => {
     var btnModo = document.getElementById('btnModo')
     var elemBody = document.getElementById('elemBody')
 
-    if (btnModo.value == `Dark Mode`) {
-        btnModo.value = "Light Mode"
+    if (btnModo.value == `-Dark Mode-`) {
+        btnModo.value = "-Light Mode-"
         elemBody.classList.toggle("modoClaro")
     } else {
-        btnModo.value = "Dark Mode"
+        btnModo.value = "-Dark Mode-"
         elemBody.classList.toggle("modoClaro")
     }
 });
 
-document.addEventListener('click', function (event) {
-    if (!event.target.closest('.cardPais')) return;
 
-    // Log the clicked element in the console
-    console.log(event.target.parentNode.childNodes[3].innerHTML);
-    abrirPaisDetalle(event.target.parentNode.childNodes[3].innerHTML)
-}, false);
-
-function abrirPaisDetalle(pais) {
-    window.open("detalle.html?name=" + pais, "_self");        
-}
 
 
 
