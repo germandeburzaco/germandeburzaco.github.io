dd7d66c0d0cdfed9eed1574625382d2c46f9e199
germandeburzaco <germandeburzaco@hotmail.com>
2021-10-10 09:42:04 -0300

    123

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
-var btnModo = document.getElementById("btnModo")
 
-btnModo.addEventListener('click', (e) => {
-    var btnModo = document.getElementById('btnModo')
-    var elemBody = document.getElementById('elemBody')
+document.addEventListener("load", () => {
+    var btnModo = document.getElementById("btnModo")
 
-    if (btnModo.value == `-Dark Mode-`) {
-        btnModo.value = "-Light Mode-"
-        elemBody.classList.toggle("modoClaro")
-    } else {
-        btnModo.value = "-Dark Mode-"
-        elemBody.classList.toggle("modoClaro")
-    }
-});
+    var iconoLight = document.getElementsById("fas")
+    console.log(iconoLight[0])
 
-window.addEventListener("load", ()=>{
+    iconoLight[0].addEventListener('click', (e) => {
+        console.log("ok")
+        var btnModo = document.getElementById('btnModo')
+        var elemBody = document.getElementById('elemBody')
 
+        if (btnModo.value == `-Dark Mode-`) {
+            btnModo.value = "-Light Mode-"
+            elemBody.classList.toggle("modoClaro")
+            iconoLight.classList.toggle("fa-sun")
+            iconoLight.classList.toggle("fa-moon")
+        } else {
+            btnModo.value = "-Dark Mode-"
+            elemBody.classList.toggle("modoClaro")
+            iconoLight.classList.toggle("fa-sun")
+            iconoLight.classList.toggle("fa-moon")
+        }
+    });
+
+})
+window.addEventListener("load", () => {
+    var iconoLight = document.getElementsByClassName("fas")
+    console.log(iconoLight[0])
     let puntos = document.getElementById("main-punto")
-
-    const puntoIntermitente = () =>{
+    const puntoIntermitente = () => {
         puntos.classList.toggle("main-punto-oculto")
         setTimeout(puntoIntermitente, 1000)
-    }    
-
+    }
     puntoIntermitente()
+
+    iconoLight[0].addEventListener('click', (e) => {
+        console.log("ok")
+        var btnModo = document.getElementById('btnModo')
+        var elemBody = document.getElementById('elemBody')
+
+        if (btnModo.value == `-Dark Mode-`) {
+            btnModo.value = "-Light Mode-"
+            elemBody.classList.toggle("modoClaro")
+            iconoLight.classList.toggle(fa - sun)
+            iconoLight.classList.toggle(fa - moon)
+        } else {
+            btnModo.value = "-Dark Mode-"
+            elemBody.classList.toggle("modoClaro")
+            iconoLight.classList.toggle(fa - sun)
+            iconoLight.classList.toggle(fa - moon)
+        }
+    });
 
 })
 
